da2f679dd34d6dc5e25717b32490ad57a776a5d0
spotsccc <chernenko.ivan.k17@gmail.com>
2021-03-25 16:26:15 +0300

    fix

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -61,6 +61,7 @@ function MyApp({Component, pageProps}) {
     return <>
         <Head>
             <title>Orbitlabs</title>
+            <link rel="shortcut icon" href="/favicon.ico" />
             <meta name="viewport" content="width=device-width, initial-scale=1"/>
         </Head>
         {
